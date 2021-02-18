8c0c36567cd559ebd960e51fb309901de503c99b
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-18 23:54:11 +0900

    投稿編集の設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -23,6 +23,19 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
   end
+  def edit
+    @tweet = Tweet.find(params[:id])
+    redirect_to root_path unless current_user.id == @tweet.user_id
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
 
  private
   def tweet_params
