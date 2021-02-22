48c9753fecd3a5b230af3a9e72aecc2e5c8bfcee
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-22 16:28:41 +0900

    テストの変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -56,7 +56,7 @@ class TweetsController < ApplicationController
 
  private
   def tweet_params
-    params.require(:tweets).permit(:title, :youtube_url, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:title, :youtube_url, :text).merge(user_id: current_user.id)
   end
 
   def group_tweet
