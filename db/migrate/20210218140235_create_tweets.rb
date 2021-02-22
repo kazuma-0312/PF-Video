65d8da034c29de6712eec93be15ea8da6b03eb52
Kazuma Sakamoto <74850915+kazuma-0312@users.noreply.github.com>
2021-02-22 20:07:11 +0900

    細かい変更

--- db/migrate/20210218140235_create_tweets.rb
+++ db/migrate/20210218140235_create_tweets.rb
@@ -1,9 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string   :title,   null: false
-      t.string   :youtube_url, null: false
-      t.text   :text,      null: false
+      t.string   :title,   null: false, default: ""
+      t.string   :youtube_url, null: false, default: ""
+      t.string   :text,      null: false, default: ""
       t.references   :user,         foreign_key: true
       t.string   :comment_id,         foreign_key: true
       t.string   :like_id,         foreign_key: true
